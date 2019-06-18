6aab1dcf62b4ff09895d9fc3be9b4f7bfd46cb17
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-06-18 16:44:38 -0500

    starting point for the view, no functionality in the handlclick at this time

---
+++ src/components/FriendCard/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>{props.name}</strong>
+          </li>
+        </ul>
+      </div>
+      <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+      </span>
+    </div>
+  );
+}
+
+export default FriendCard;
